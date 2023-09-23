3e05c849144bc3e27f5ae7a7f9c7e5817be434e4
Tishalovich Maxim <tishalovichm@gmail.com>
2023-09-23 21:57:17 +0300

    sample bean

---
+++ wt-lab-2/src/main/java/by/your_anime_list/bean/Anime.java
@@ -0,0 +1,9 @@
+package by.your_anime_list.bean;
+
+public class Anime {
+    private int id;
+    private String name;
+    private String authorName;
+    private float rating;
+    private String imagePath;
+}
---
+++ wt-lab-2/src/main/java/by/your_anime_list/bean/User.java
@@ -0,0 +1,4 @@
+package by.your_anime_list.bean;
+
+public class User {
+}
